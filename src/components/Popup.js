927f3a52e209b55e9fc15441e1cddac65bdb620a
Dora Fraeman Caswell <dorafraeman@gmail.com>
2020-03-19 22:15:32 -0400

    More notes on missing work

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -9,6 +9,9 @@ import { CloseButton } from '../styles/button'
  * Relies on children prop to render content in the popup
  * @props {function} toggleAction - function to close the popup
  * @props {*} children - content for the popup
+ * 
+ * NOTE: In production environment, a component like this would need ARIA roles and other
+ * accessibility attention
  */
 
 class Popup extends Component{
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,8 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
+/**
+ * NOTE : Unfortuntely, I wasn't able to write any tests for this app. I think testing is important.
+ */
 import '@testing-library/jest-dom/extend-expect';
